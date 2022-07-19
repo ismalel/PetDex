1ffe47827d9ab3cd99f58776c67a6993bc9c4994
xjwalker <jesus.walker3000@gmail.com>
2022-07-19 16:49:59 -0700

    Revert "changed sizes"
    This reverts commit e174d837

--- app/src/main/java/com/ismalel/petdex/MainActivity.kt
+++ app/src/main/java/com/ismalel/petdex/MainActivity.kt
@@ -52,18 +52,18 @@ fun MessageCard(msg: Message) {
             contentDescription = "Contact profile picture",
             modifier = Modifier
                 // Set image size to 40 dp
-                .size(50.dp)
+                .size(40.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)
         )
 
         // Add a horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(10.dp))
+        Spacer(modifier = Modifier.width(8.dp))
 
         Column {
             Text(text = msg.author)
             // Add a vertical space between the author and message texts
-            Spacer(modifier = Modifier.height(6.dp))
+            Spacer(modifier = Modifier.height(4.dp))
             Text(text = msg.body)
         }
     }
