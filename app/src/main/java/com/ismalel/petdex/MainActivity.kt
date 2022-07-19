6b8d7fb749c2148e58c8a7307b61982cd4e282ec
Ismael Ochoa <ismael.ochoa@mindstronghealth.com>
2022-07-19 12:00:10 -0500

    Added format with image

--- app/src/main/java/com/ismalel/petdex/MainActivity.kt
+++ app/src/main/java/com/ismalel/petdex/MainActivity.kt
@@ -3,7 +3,6 @@ package com.ismalel.petdex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -11,6 +10,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ismalel.petdex.ui.theme.PetDexTheme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.ismalel.petdex.data.entity.Message
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("ADsadsdasd")
+                    MessageCard(
+                        msg = Message("Ismael", "Krispky kreme")
+                    )
                 }
             }
         }
@@ -34,10 +43,36 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    PetDexTheme {
-        Greeting("Adsdf")
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.pet_profile),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
     }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Ismael", "Krispky kreme")
+    )
 }
---
+++ app/src/main/java/com/ismalel/petdex/data/entity/Message.kt
@@ -0,0 +1,3 @@
+package com.ismalel.petdex.data.entity
+
+data class Message(val author: String, val body: String)
